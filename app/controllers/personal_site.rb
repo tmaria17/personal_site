5494532b3d8d41195bb0f13132d3a89c88687f04
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-08-06 16:34:42 -0600

    creates method self.call in personalsite class and saves capybara path.

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -1,3 +1,9 @@
+require 'rack'
+
 class PersonalSite
 
+  def self.call(env)
+    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
+  end
+  
 end
